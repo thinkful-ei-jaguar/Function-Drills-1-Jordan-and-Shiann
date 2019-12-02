c5b75c3b77dbdeb9797ad3959520b5d2c208478e
carnagecreations <30275496+carnagecreations@users.noreply.github.com>
2019-12-02 14:40:53 -0700

    commit

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-console.log('Hi my name is Shiann and I\'m 32');
+//console.log('Hi my name is Shiann and I\'m 32');
 
-function createGreeting {
-    console.log('Hi my name is Shiann and I\'m 32');
+//function createGreeting() {
+    //console.log('Hi my name is Shiann and I\'m 32');
+//}
+//createGreeting();
+
+function createGreeting() {
+    return 'Hi my name is Shiann and I\'m 32';
 }
+const greeting1 = createGreeting();
+console.log(greeting1);
 
-createGreeting();
